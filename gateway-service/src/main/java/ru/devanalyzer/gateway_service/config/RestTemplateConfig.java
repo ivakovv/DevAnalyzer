2182e952ea211445512e5e5642fc7f2edc51f2fd
ivakov <ivakov.andrey.2004@mail.ru>
2026-03-12 23:06:36 +0300

    add Kubernetes configuration and remove Eureka

--- gateway-service/src/main/java/ru/devanalyzer/gateway_service/config/RestTemplateConfig.java
+++ gateway-service/src/main/java/ru/devanalyzer/gateway_service/config/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package ru.devanalyzer.gateway_service.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +14,6 @@ public class RestTemplateConfig {
     private final UserContextInterceptor userContextInterceptor;
 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setInterceptors(List.of(userContextInterceptor));
