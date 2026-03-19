542b2034c23eb45a9267de45321bc96244999ab6
ivakov <ivakov.andrey.2004@mail.ru>
2026-03-19 20:53:41 +0300

    initial commit for analyzer-service

---
+++ analyzer-service/src/main/java/ru/devanalyzer/analyzer_service/AnalyzerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.devanalyzer.analyzer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerServiceApplication.class, args);
+	}
+
+}
