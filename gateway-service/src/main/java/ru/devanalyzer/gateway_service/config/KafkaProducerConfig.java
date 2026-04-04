8b4027c76a4fd520324bd29c0d14ee7d25dc1ab6
ivakov <ivakov.andrey.2004@mail.ru>
2026-04-04 15:52:10 +0300

    refactor(gateway): refactor kafka config

--- gateway-service/src/main/java/ru/devanalyzer/gateway_service/config/KafkaProducerConfig.java
+++ gateway-service/src/main/java/ru/devanalyzer/gateway_service/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     @Bean
@@ -26,6 +26,11 @@ public class KafkaProducerConfig {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.RETRIES_CONFIG, 3);
+        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        config.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+        
         return new DefaultKafkaProducerFactory<>(config);
     }
 
