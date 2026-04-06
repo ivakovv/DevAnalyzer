9b27bca8a0b03704c384964e5c22044b1007acef
m.sorokin <kolibrimax05@mail.ru>
2026-04-06 23:22:59 +0300

    fix bug

--- analyzer-service/src/main/java/ru/devanalyzer/analyzer_service/repository/CommitHeatMapRepository.java
+++ analyzer-service/src/main/java/ru/devanalyzer/analyzer_service/repository/CommitHeatMapRepository.java
@@ -1,10 +1,15 @@
 package ru.devanalyzer.analyzer_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import ru.devanalyzer.analyzer_service.entity.CommitHeatmapEntity;
 import java.util.List;
 
 public interface CommitHeatMapRepository extends JpaRepository<CommitHeatmapEntity, Long> {
     List<CommitHeatmapEntity> findByGithubId(Long githubId);
+
+    @Modifying
+    @Query("DELETE FROM CommitHeatmapEntity c WHERE c.githubId = :githubId")
     void deleteByGithubId(Long githubId);
 }
