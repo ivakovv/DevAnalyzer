46fed7e098e668ef4696f122ea363ff452597d74
ivakov <ivakov.andrey.2004@mail.ru>
2026-04-12 15:25:40 +0300

    add OpenApiConfig for analyzer-service

---
+++ analyzer-service/src/main/java/ru/devanalyzer/analyzer_service/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package ru.devanalyzer.analyzer_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${API_SERVER_URL}") String serverUrl) {
+        return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl));
+    }
+}
